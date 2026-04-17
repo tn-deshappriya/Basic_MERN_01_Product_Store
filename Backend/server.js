c2759053388208add370a0e70382b8782c194103
Mr.No-Hat <thiniranilushandeshappriya@gmail.com>
2026-04-17 23:14:33 +0530

    Create:- MongoDB Database Connection.

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,16 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+
+dotenv.config();
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('server is running');
-})
+app.get('/products', (req, res) => { });
 
 app.listen(3001, () => {
+    connectDB();
     console.log('Server is running on localhost:3001');
-})
+});
+
+// mongodb+srv://thiniranilushandeshappriya_db_user:E6EGenqSrJtcg40s@cluster1.xdfa3aj.mongodb.net/?appName=Cluster1
