d6b2ae12073be66f15008a404edba859c3583d46
Mr.No-Hat <thiniranilushandeshappriya@gmail.com>
2026-04-19 20:52:21 +0530

    Create:- Post APi endpoint for save product items.

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,29 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
+import Product from './models/product.model.js';
 
 dotenv.config();
 
 const app = express();
 
-app.post('/products', (req, res) => { });
+app.use(express.json()); // allows us to accept JSON data in the req.body.
+
+app.post('/api/products', async (req, res) => {
+    const product = req.body; // Assuming the product data is sent in the request body as JSON
+
+    if (!product.name || !product.price || !product.image) {
+        return res.status(400).json({ success: false, message: 'All fields are required' });
+    }
+    const newProduct = new Product(product);
+    try {
+        await newProduct.save();
+        res.status(201).json({ success: true, data: newProduct });
+    } catch (error) {
+        console.error('Error in creating product:', error);
+        res.status(500).json({ success: false, message: 'Error saving product' });
+    }
+});
 
 app.listen(3001, () => {
     connectDB();
