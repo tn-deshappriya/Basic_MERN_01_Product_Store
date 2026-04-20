7833a61ec0884b25df4a7059503b92c21aac9a75
Mr.No-Hat <thiniranilushandeshappriya@gmail.com>
2026-04-20 18:28:29 +0530

    Create:- Put APi endpiont for update item data in products.

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import Product from './models/product.model.js';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
@@ -30,18 +31,35 @@ app.post('/api/products', async (req, res) => {
         await newProduct.save();
         res.status(201).json({ success: true, data: newProduct });
     } catch (error) {
-        console.error('Error in creating product:', error);
+        console.log('Error in creating product:', error.message);
         res.status(500).json({ success: false, message: 'Error saving product' });
     }
 });
+
+app.put('/api/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const updatedData = req.body; // Assuming the updated product data is sent in the request body as JSON
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ success: false, message: 'Invalid product ID' });
+    }
+
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(id, updatedData, { new: true });
+        res.status(200).json({ success: true, data: updatedProduct });
+    } catch (error) {
+        console.log('Error in updating product:', error.message);
+        res.status(500).json({ success: false, message: 'Server Error' });
+    }
+})
 
 app.delete('/api/products/:id', async (req, res) => {
     const { id } = req.params;
     try {
         await Product.findByIdAndDelete(id);
-        res.status(200).json({ success: true, massage: 'Product deleted successfully' });
+        res.status(200).json({ success: true, message: 'Product deleted successfully' });
     } catch (error) {
-        console.error('Error in deleting product:', error);
+        console.log('Error in deleting product:', error.message);
         res.status(404).json({ success: false, message: 'Product not found' });
     }
 });
