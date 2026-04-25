39677df207ed6f7bf30d286ad98b232b8a4cda0d
Mr.No-Hat <thiniranilushandeshappriya@gmail.com>
2026-04-25 09:15:38 +0530

    Create:- BrowserRouter wrapper in main.jsx file.

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,12 +1,15 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { StrictMode } from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <BrowserRouter>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
