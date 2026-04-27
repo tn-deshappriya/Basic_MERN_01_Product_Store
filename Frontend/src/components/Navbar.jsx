49091e45f5b74168eaafe0baabaeb32fd93f15cd
Mr.No-Hat <thiniranilushandeshappriya@gmail.com>
2026-04-27 22:06:49 +0530

    Fix:- bgGradient in Navbar.jsx Product Store Text line v3 to v2 for work visible.

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
                     fontWeight={"bold"}
                     textTransform={"uppercase"}
                     textAlign={"center"}
-                    bgGradient="to-r" gradientFrom="cyan.400" gradientTo="blue.500"
+                    bgGradient="linear(to-r, cyan.400, blue.500)"
                     bgClip={"text"}>
                     <Link to={"/"}>Product Store 🛒</Link>
                 </Text>
