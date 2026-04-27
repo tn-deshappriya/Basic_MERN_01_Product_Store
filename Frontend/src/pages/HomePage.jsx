fd46a01a75aad71cb2727fb2bde17592b90de867
Mr.No-Hat <thiniranilushandeshappriya@gmail.com>
2026-04-28 04:51:24 +0530

    Create:- Home page create product titles.

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,7 +1,27 @@
-
+import { Container, VStack, Text } from "@chakra-ui/react"
+import { Link } from "react-router-dom"
 const HomePage = () => {
     return (
-        <div>HomePage</div>
+        <Container maxW="container.xl" py={12}>
+            <VStack spacing={8}>
+                <Text
+                    fontSize={"30"}
+                    fontWeight={"bold"}
+                    bgGradient={"linear(to-r, cyan.400, blue.500)"}
+                    bgClip={"text"}
+                    textAlign={"center"}>
+                    Current Products 🚀
+                </Text>
+                <Text fontSize={"xl"} alignItems={"center"} fontWeight={"bold"} color={'gray.500'}>
+                    No Product Found 😢 {" "}
+                    <Link to={"/create"}>
+                        <Text as={"span"} color="blue.500" _hover={{ direction: "underline" }}>
+                            Create Product
+                        </Text>
+                    </Link>
+                </Text>
+            </VStack>
+        </Container>
     )
 }
 
