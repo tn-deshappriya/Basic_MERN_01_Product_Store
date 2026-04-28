3c785a86b104ad2772779019371d51aada5b646e
Mr.No-Hat <thiniranilushandeshappriya@gmail.com>
2026-04-28 12:53:11 +0530

    Create:- Delete Product function and connection.

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,6 +1,13 @@
-import { Container, VStack, Text } from "@chakra-ui/react"
+import { Container, VStack, Text, SimpleGrid } from "@chakra-ui/react"
+import { useEffect } from "react"
 import { Link } from "react-router-dom"
+import { useProductStore } from "../store/product"
+import ProductCard from "../components/ProductCard.jsx"
+
 const HomePage = () => {
+    const { fetchProducts, products } = useProductStore();
+    useEffect(() => { fetchProducts() }, [fetchProducts]);
+    console.log("products", products);
     return (
         <Container maxW="container.xl" py={12}>
             <VStack spacing={8}>
@@ -12,14 +19,21 @@ const HomePage = () => {
                     textAlign={"center"}>
                     Current Products 🚀
                 </Text>
-                <Text fontSize={"xl"} alignItems={"center"} fontWeight={"bold"} color={'gray.500'}>
-                    No Product Found 😢 {" "}
-                    <Link to={"/create"}>
-                        <Text as={"span"} color="blue.500" _hover={{ direction: "underline" }}>
-                            Create Product
-                        </Text>
-                    </Link>
-                </Text>
+
+                <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={10} w={"full"}>
+                    {products.map((product) => (
+                        <ProductCard key={product._id} product={product} />
+                    ))}
+                </SimpleGrid>
+                {products.length === 0 && (
+                    <Text fontSize={"xl"} alignItems={"center"} fontWeight={"bold"} color={'gray.500'}>
+                        No Product Found 😢 {" "}
+                        <Link to={"/create"}>
+                            <Text as={"span"} color="blue.500" _hover={{ direction: "underline" }}>
+                                Create Product
+                            </Text>
+                        </Link>
+                    </Text>)}
             </VStack>
         </Container>
     )
